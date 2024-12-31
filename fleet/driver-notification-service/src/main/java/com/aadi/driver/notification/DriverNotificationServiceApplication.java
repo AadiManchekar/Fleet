6f45f21d5fe596f14032326827ccc0ffd4fb2b0f
AadiManchekar <aadimanchekar2002@gmail.com>
2025-01-01 04:28:21 +0530

    Init code for DriverNotificationService

---
+++ fleet/driver-notification-service/src/main/java/com/aadi/driver/notification/DriverNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aadi.driver.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverNotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DriverNotificationServiceApplication.class, args);
+    }
+
+}
---
+++ fleet/driver-notification-service/src/test/java/com/aadi/driver/notification/DriverNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aadi.driver.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriverNotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
