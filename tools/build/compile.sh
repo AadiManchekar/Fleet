a253beadc980f3a7ce0c80fd36459a14136dbb2f
AadiManchekar <aadimanchekar2002@gmail.com>
2025-02-23 16:35:14 +0530

    resolve dependencies in seperate stages

--- tools/build/compile.sh
+++ tools/build/compile.sh
@@ -20,7 +20,7 @@ print_line_break
 
 echo -e "\033[0;32mACTION: mvn install\033[0m"  # Dark green
 print_line_break
-mvn install -X
+mvn install
 print_line_break
 
 echo -e "\033[0;32mSUCCESS\033[0m"  # Dark green
