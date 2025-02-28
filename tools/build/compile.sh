154014f3e3b1fd2e810570b908576203f9a295a3
AadiManchekar <aadimanchekar2002@gmail.com>
2025-02-28 23:51:37 +0530

    Enhanced compile.sh with success and error handling functions

--- tools/build/compile.sh
+++ tools/build/compile.sh
@@ -4,11 +4,24 @@ function print_line_break {
     echo "==========================================================================="
 }
 
+function print_success {
+    print_line_break
+    echo -e "\033[0;32mSUCCESS\033[0m"  # Dark green
+    print_line_break
+    exit 0
+}
+
+function print_error {
+    print_line_break
+    echo -e "\033[0;31mERROR\033[0m"  # Red
+    print_line_break
+    exit 1
+}
+
 print_line_break
 cd $(dirname "$0")/../../
 echo -e "\033[0;33mDIRECTORY: $(pwd)\033[0m"  # Dark yellow
 print_line_break
-
 
 echo -e "\033[0;32mACTION: mvn clean\033[0m"  # Dark green
 print_line_break
@@ -17,12 +30,7 @@ echo "Cleaning up old build files..."
 mvn clean
 print_line_break
 
-
-echo -e "\033[0;32mACTION: mvn install\033[0m"  # Dark green
+echo -e "\033[0;32mACTION: mvn install -DskipTests\033[0m"  # Dark green
 print_line_break
-mvn install
-print_line_break
-
-echo -e "\033[0;32mSUCCESS\033[0m"  # Dark green
-print_line_break
+mvn install -DskipTests && print_success || print_error
 
