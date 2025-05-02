1d5b6d56e4c5426995571b1ea2bcf03b43c5989d
AadiManchekar <aadimanchekar2002@gmail.com>
2025-05-02 23:02:56 +0530

    Add build scripts for compiling and formatting code with error handling

---
+++ tools/build/compile.sh
@@ -0,0 +1,47 @@
+#!/bin/bash -e
+
+function print_line_break {
+    echo "==========================================================================="
+}
+
+function print_success {
+    print_line_break
+    echo -e "\033[0;32mSUCCESS\033[0m"  # Dark green
+    print_line_break
+    exit 0
+}
+
+function print_error {
+    print_line_break
+    echo -e "\033[0;31mERROR\033[0m"  # Red
+    print_line_break
+    exit 1
+}
+
+print_line_break
+cd $(dirname "$0")/../../
+echo -e "\033[0;33mDIRECTORY: $(pwd)\033[0m"  # Dark yellow
+print_line_break
+
+# MVN CLEAN
+echo -e "\033[0;32mACTION: mvn clean\033[0m"  # Dark green
+print_line_break
+echo "Cleaning up old build files..."
+mvn clean
+print_line_break
+
+# If running in GitHub CI, skip docker compose up
+if [ "$GITHUB_ACTIONS" != "true" ]; then
+    echo -e "\033[0;32mACTION: docker compose up -d\033[0m"  # Dark green
+    print_line_break
+    echo "Starting up the docker compose..."
+    cd tools/docker
+    docker-compose up -d
+    cd ../..
+    print_line_break
+fi
+
+# MVN INSTALL
+echo -e "\033[0;32mACTION: mvn install\033[0m"  # Dark green
+print_line_break
+mvn install && print_success || print_error
