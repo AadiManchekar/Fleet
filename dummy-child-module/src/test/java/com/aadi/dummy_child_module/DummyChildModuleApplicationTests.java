5e7d3562f02f080225b4bde8d4c05deabc18e349
AadiManchekar <aadimanchekar2002@gmail.com>
2025-05-18 01:42:48 +0530

    Add dummy child module with initial configuration and dependencies

---
+++ dummy-child-module/src/test/java/com/aadi/dummy_child_module/DummyChildModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aadi.dummy_child_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DummyChildModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
