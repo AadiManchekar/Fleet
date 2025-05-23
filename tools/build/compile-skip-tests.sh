0f677e11549eb3224f54b7c08ed3dec3581e231c
AadiManchekar <aadimanchekar2002@gmail.com>
2025-05-23 20:48:04 +0530

    Refactor compile-skip-tests.sh to build child module's docker image

--- tools/build/compile-skip-tests.sh
+++ tools/build/compile-skip-tests.sh
@@ -6,30 +6,29 @@ function print_line_break {
 
 function print_success {
     print_line_break
-    echo -e "\033[0;32mSUCCESS\033[0m"  # Dark green
+    echo -e "\033[0;32mSUCCESS\033[0m" # Dark green
     print_line_break
     exit 0
 }
 
 function print_error {
     print_line_break
-    echo -e "\033[0;31mERROR\033[0m"  # Red
+    echo -e "\033[0;31mERROR\033[0m" # Red
     print_line_break
     exit 1
 }
 
 print_line_break
 cd $(dirname "$0")/../../
-echo -e "\033[0;33mDIRECTORY: $(pwd)\033[0m"  # Dark yellow
+echo -e "\033[0;33mDIRECTORY: $(pwd)\033[0m" # Dark yellow
 print_line_break
 
-echo -e "\033[0;32mACTION: mvn clean\033[0m"  # Dark green
-print_line_break
-# Clean up old build files
-echo "Cleaning up old build files..."
-mvn clean
+echo -e "\033[0;32mACTION: Building dumm-child-module, SKIP_TEST=true\033[0m" # Dark green
 print_line_break
 
-echo -e "\033[0;32mACTION: mvn install -DskipTests\033[0m"  # Dark green
-print_line_break
-mvn install -DskipTests && print_success || print_error
+docker build \
+    --build-arg MODULE_NAME=dummy-child-module \
+    --build-arg PORT=9426 \
+    --build-arg SKIP_TESTS=true \
+    -t fleet/dummy-child-module \
+    -f tools/docker/Dockerfile . && print_success || print_error
