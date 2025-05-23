df0bffd4ddeea191098c88ee03cf8775acca035b
AadiManchekar <aadimanchekar2002@gmail.com>
2025-05-23 20:56:01 +0530

    Refactor compile-skip-tests.sh to use color code as variables

--- tools/build/compile-skip-tests.sh
+++ tools/build/compile-skip-tests.sh
@@ -1,29 +1,35 @@
 #!/bin/bash -e
 
+# Define color codes as variables
+GREEN="\033[0;32m"
+RED="\033[0;31m"
+YELLOW="\033[0;33m"
+RESET="\033[0m" # Reset to default color
+
 function print_line_break {
     echo "==========================================================================="
 }
 
 function print_success {
     print_line_break
-    echo -e "\033[0;32mSUCCESS\033[0m" # Dark green
+    echo -e "${GREEN}SUCCESS${RESET}"
     print_line_break
     exit 0
 }
 
 function print_error {
     print_line_break
-    echo -e "\033[0;31mERROR\033[0m" # Red
+    echo -e "${RED}ERROR${RESET}"
     print_line_break
     exit 1
 }
 
 print_line_break
 cd $(dirname "$0")/../../
-echo -e "\033[0;33mDIRECTORY: $(pwd)\033[0m" # Dark yellow
+echo -e "${YELLOW}DIRECTORY: $(pwd)${RESET}"
 print_line_break
 
-echo -e "\033[0;32mACTION: Building dumm-child-module, SKIP_TEST=true\033[0m" # Dark green
+echo -e "${GREEN}ACTION: Building dummy-child-module, SKIP_TEST=true${RESET}"
 print_line_break
 
 docker build \
